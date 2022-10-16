9c16c281a1819cddbb32490600de4821d229a97f
Reece Devenney <reecedevenney@gmail.com>
2022-10-16 14:37:59 -0400

    connected sql tables

--- models/index.js
+++ models/index.js
@@ -1,3 +1,29 @@
 const User = require('./User')
 const Post = require('./Post')
 const Comment = require('./Comment')
+
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+})
+
+Post.belongsTo(User, {
+    foreginKey: 'user_id'
+})
+
+User.hasMany(Comment, {
+    foreignKey: 'user_id'
+})
+
+Comment.belongsTo(User, {
+    foreginKey: 'user_id'
+})
+
+Post.hasMany(Comment, {
+    foreignKey: 'post_id'
+})
+
+Comment.belongsTo(Post, {
+    foreginKey: 'post_id'
+})
+
+module.exports = { User, Post, Comment };
